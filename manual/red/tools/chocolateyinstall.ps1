6d03cc62a46d04e184971b905d465ac7604b510d
Dave Andersen <d.andersen@gmail.com>
2016-02-09 00:11:24 -0800

    add Red package
    installs external Red 0.5.4

---
+++ manual/red/tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName = 'red'
+$url = 'http://static.red-lang.org/dl/win/red-054.exe'
+$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$redPath = Join-Path $scriptPath 'red.exe'
+
+Get-ChocolateyWebFile $packageName "$redPath" $url
