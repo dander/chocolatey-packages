02708549ab2d65dfbcb6fb2efafcfb48baa8e2dd
Dave Andersen <d.andersen@gmail.com>
2016-03-26 01:56:51 -0700

    update red package to 0.6.0

--- manual/red/tools/chocolateyinstall.ps1
+++ manual/red/tools/chocolateyinstall.ps1
@@ -1,6 +1,12 @@
-﻿$packageName = 'red'
-$url = 'http://static.red-lang.org/dl/win/red-054.exe'
-$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$redPath = Join-Path $scriptPath 'red.exe'
 
-Get-ChocolateyWebFile $packageName "$redPath" $url
+$scriptPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$webFileArgs = @{
+  packageName = 'red'
+  url = 'http://static.red-lang.org/dl/win/red-060.exe'
+  fileFullPath = Join-Path $scriptPath 'red.exe'
+  checksum = 'a4b45da7c037480e49175df261fc2fa61ef2ab02'
+  checksumType = 'sha1'
+}
+
+Get-ChocolateyWebFile @webFileArgs
